1f6fcba80757c531de8ed1710a88919a2e539098
Abhishek Kumar <abhishekkgf999@gmail.com>
2026-09-09 17:12:18 +0530

    Create basic structure of flash card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from '../Button/Button'
+import './Card.css'
+
+const Card = () => {
+  return (
+    <div className='flash-card'>
+        <div className='question-answer'>
+            <p>This contains question and answer</p>
+        </div>
+        <div className='button'>
+            <Button/>
+            <Button/>
+            <Button/>
+        </div>
+    </div>
+  )
+}
+
+export default Card
