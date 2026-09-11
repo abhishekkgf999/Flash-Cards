03e9768db61db2beb09acb7c54b2978d5c22e653
Abhishek Kumar <abhishekkgf999@gmail.com>
2026-09-11 16:33:15 +0530

    Fetch data from flashcard dataset and display

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Button from '../Button/Button'
 import './Card.css'
+import flashcards from '../../assets/flashcard'
 
 const Card = () => {
+  const currentFlashCard = flashcards[0];
   return (
     <div className='flash-card'>
         <div className='question-answer'>
-            <p>This contains question and answer</p>
+            <p>{currentFlashCard.question}</p>
         </div>
         <div className='button'>
             <Button label = "< Prev"/>
