473fac50414b1c58112b72b706e8d57ad965f1f3
Abhishek <113373076+abhishekkgf999@users.noreply.github.com>
2026-09-11 21:33:36 +0530

    Implement Show/Hide button function (#17)

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,21 +2,30 @@ import React from 'react'
 import Button from '../Button/Button'
 import './Card.css'
 import flashcards from '../../assets/flashcard'
+import { useState } from "react";
 
 const Card = () => {
   const currentFlashCard = flashcards[0];
+  const [isAnswerVisible, setVisible] = useState(false);
   return (
-    <div className='flash-card'>
-        <div className='question-answer'>
-            <p>{currentFlashCard.question}</p>
-        </div>
-        <div className='button'>
-            <Button label = "< Prev"/>
-            <Button label = "Show Answer"/>
-            <Button label = "Next >"/>
-        </div>
+    <div className="flash-card">
+      <div className="question-answer">
+        {isAnswerVisible ? (
+          <p>{currentFlashCard.answer}</p>
+        ) : (
+          <p>{currentFlashCard.question}</p>
+        )}
+      </div>
+      <div className="button">
+        <Button label="< Prev" />
+        <Button
+          label={isAnswerVisible ? "Hide Answer" : "Show Answer"}
+          event={() => setVisible(!isAnswerVisible)}
+        />
+        <Button label="Next >" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Card
