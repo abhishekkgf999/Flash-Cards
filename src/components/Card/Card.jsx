5b69e9ebedd7b6e80300e4d756d1c8eabd4d53ba
Abhishek Kumar <abhishekkgf999@gmail.com>
2026-09-12 21:15:50 +0530

    Implement Next/Previous button navigation

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,8 +4,21 @@ import flashcards from '../../assets/flashcard'
 import { useState } from "react";
 
 const Card = () => {
-  const currentFlashCard = flashcards[0];
   const [isAnswerVisible, setVisible] = useState(false);
+  const [currentIndex, setIndex] = useState(0);
+  const currentFlashCard = flashcards[currentIndex];
+  const handleNext = () => {
+    if (currentIndex < flashcards.length - 1) {
+      setIndex(currentIndex + 1);
+      setVisible(false);
+    }
+  };
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setIndex(currentIndex - 1);
+      setVisible(false);
+    }
+  };
   return (
     <div className="flash-card">
       <div className="question-answer">
@@ -16,12 +29,12 @@ const Card = () => {
         )}
       </div>
       <div className="button">
-        <Button label="< Prev" />
+        <Button label="< Prev" event={handlePrev} />
         <Button
           label={isAnswerVisible ? "Hide Answer" : "Show Answer"}
           event={() => setVisible(!isAnswerVisible)}
         />
-        <Button label="Next >" />
+        <Button label="Next >" event={handleNext} />
       </div>
     </div>
   );
