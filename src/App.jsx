af57f710735e477055cd6dffc98d79433fb19f53
RunAtTekky <varunrawat343@gmail.com>
2026-09-12 17:09:22 +0530

    Remove unnecessary React import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Card from './components/Card/Card'
 import ProgressBar from './components/Progress-Bar/ProgressBar'
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Button from '../Button/Button'
 import './Card.css'
 import flashcards from '../../assets/flashcard'
